7f86897fdd90221e90f3387b544906c6ee1df975
Mark Karpov <markkarpov92@gmail.com>
2021-08-16 21:58:10 +0200

    Rough layout of the problem

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,40 @@ module Main (main) where
 
 import Codec.Picture
 
+paletteSize :: Int
+paletteSize = 5
+
 main :: IO ()
 main = do
   e <- readJpeg "input.jpeg"
   case e of
     Left str -> putStrLn str
     Right dynamicImage -> do
-      saveJpgImage 100 "output.jpeg" dynamicImage
-      putStrLn "I'm all done!"
-    -- Right _ ->
-    --   putStrLn "Unexpected color scheme of the image"
+      let imageRGB8 = convertRGB8 dynamicImage
+          palette = extractPalette paletteSize imageRGB8
+          imageWithPalette = ImageRGB8 (attachPalette palette imageRGB8)
+      saveJpgImage 100 "output.jpeg" imageWithPalette
+
+-- | A color palette.
+newtype ColorPalette = ColorPalette [PixelRGB8]
+  deriving (Eq, Show)
+
+-- | Extract the given number of primary colors from an image.
+extractPalette ::
+  -- | Number of colors to extract
+  Int ->
+  -- | The input image
+  Image PixelRGB8 ->
+  -- | The extracted palette
+  ColorPalette
+extractPalette = undefined -- TODO
+
+-- | Attach the given color palette to the given image.
+attachPalette ::
+  -- | Color palette
+  ColorPalette ->
+  -- | Original image
+  Image PixelRGB8 ->
+  -- | Image with the palette attached
+  Image PixelRGB8
+attachPalette = undefined -- TODO
